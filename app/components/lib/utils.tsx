8705cc71bb0b7c022ec4c62e1b36cb3928e7b580
LeoAnders <leonardoanders15@gmail.com>
2024-12-28 16:55:45 -0300

    Criar seção de `Pricing` e aplicar em `pages.tsx`

--- app/components/lib/utils.tsx
+++ app/components/lib/utils.tsx
@@ -1,5 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+export const TRIAL_DAYS = 3;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
+import Pricing from "./components/landing-page/pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
+      <Pricing />
 
       {/* <VideoExplanation />
       <Pricing />
